7bb305163eecb023372cd6e2202fb4c6679ebd32
harish <160316114+muthumuruganharish@users.noreply.github.com>
2026-02-26 05:18:58 +0530

    Update profileController.js

--- backend/Controller/profileController.js
+++ backend/Controller/profileController.js
@@ -4,7 +4,7 @@ const profileAdd = async (req, res) => {
 
     try {
 
-        const user = await data.findById(req.userId).select("-password")
+        const user = await data.findById(req.userId).select("-password")//.select("-password") means it will not send password to frontend for safety purpose
 
         if (!user) {
             return res.status(404).json({ message: "User not found" })
